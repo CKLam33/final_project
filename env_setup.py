1c6269dca7312db896db25c54a6dbaca71b7d954
Gary Lam <gary199233@hotmail.com>
2025-02-16 20:56:23 +0800

    Add files via upload
    Setup for Gymnasium environment

---
+++ env_setup.py
@@ -0,0 +1,7 @@
+GBA_ROM = "./GBA_ROM/Rockman Zero 3 (Japan).gba"
+GBA_SAV = "./GBA_ROM/Rockman Zero 3 (Japan).sav"
+RENDER_MODE = "rgb_array"
+OBS_TYPE = "rgb"
+FRAMESKIP = 3
+SILENCE = True
+RESIZE = True

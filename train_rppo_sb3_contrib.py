d2b6e19e43286f4b91c14db599283ef18ae29d25
Lam Chi Kin <gary199233@hotmail.com>
2025-02-25 00:13:32 +0800

    Use default setting but change Activaton fn

--- train_rppo_sb3_contrib.py
+++ train_rppo_sb3_contrib.py
@@ -1,15 +1,23 @@
+from datetime import datetime
+from pathlib import Path
 import sys
 from typing import Any, Dict, Tuple, Union
 
 from env_setup import *
-from gymnasium.wrappers import ResizeObservation
 import mlflow
 import numpy as np
 from RMZ3Env import make_RMZ3Env
+from stable_baselines3.common.callbacks import CheckpointCallback
 from stable_baselines3.common.vec_env import SubprocVecEnv
 from stable_baselines3.common.logger import HumanOutputFormat, KVWriter, Logger
-from sb3_contrib import RecurrentPPO, MaskablePPO
+from sb3_contrib import RecurrentPPO
+import torch
+import torch.nn as nn
 
+torch.set_num_threads(NUM_ACTORS)
+
+PATH = Path("RL/RecurrentPPO")
+PATH.mkdir(parents=True, exist_ok=True)
 
 class MLflowOutputFormat(KVWriter):
     """
@@ -34,8 +42,8 @@ class MLflowOutputFormat(KVWriter):
                 if not isinstance(value, str):
                     mlflow.log_metric(key, value, step)
 
-loggers = Logger(
-    folder="./RL-model/",
+logger = Logger(
+    folder=None,
     output_formats=[HumanOutputFormat(sys.stdout), MLflowOutputFormat()],
 )
 
@@ -46,39 +54,50 @@ def make_env():
             gba_rom = GBA_ROM,
             gba_sav = GBA_SAV,
             render_mode = RENDER_MODE,
-            obs_type = OBS_TYPE,
             frameskip = FRAMESKIP,
+            max_episode_steps = MAX_STEPS,
             mgba_silence = SILENCE,
-            resize = RESIZE,
+            to_resize = RESIZE,
+            to_grayscale = True
             )
         return env
     return _init
 
-envs = [make_env() for _ in range(16)]
+envs = [make_env() for _ in range(POPSIZE)]
 
 envs = SubprocVecEnv(envs, start_method="fork")
 
-policy_kargs = {
-    "lstm_hidden_size": 128,
-    "n_lstm_layers": 3,
+policy_kwargs = {
+    "lstm_hidden_size": HIDDEN_SIZE,
+    "n_lstm_layers": NUM_LAYERS,
+    "activation_fn": nn.ReLU,
 }
 
-n_steps = (100 * 3) // 16 # popluation size * episode per generation / number of actors
+checkpoint_callback = CheckpointCallback(
+  save_freq = 1080,
+  save_path = PATH.joinpath("/checkpoints/"),
+  name_prefix = "rppo_model",
+)
 
-with mlflow.start_run():
 # Setup agent and train
-    agent = RecurrentPPO("CnnLstmPolicy",
-                        envs,
-                        n_steps = n_steps,
-                        learning_rate = 1e-4,
-                        ent_coef = 0.001,
-                        target_kl = 1e-4,
-                        verbose = 2)
+agent = RecurrentPPO("CnnLstmPolicy",
+                    envs,
+                    verbose = 1,
+                    policy_kwargs = policy_kwargs,
+                    )
 
-    # Time steps per episode = frames to finish one checkpoint / frames (frame skip + 1 rendered) per timestep
-    timesteps_per_episode = (2 * 60 * 60) // (FRAMESKIP + 1)
-    timesteps = 500 * 3 * timesteps_per_episode
-    agent.set_logger(loggers)
-    agent.learn(total_timesteps = timesteps, progress_bar=True, log_interval=1)
-    agent.save(path="./RecurrentPPO_RMZ3")
-    envs.close()
+timesteps = MAX_STEPS * GENERATIONS # (Average gameplay duration over Frameskip) * generations
+agent.set_logger(logger)
+start_time = datetime.now()
+agent.learn(total_timesteps = TOTAL_TIMESTEPS,
+            progress_bar=True,
+            log_interval=10,
+            callback=checkpoint_callback)
+
+agent.save(path = PATH.joinpath("/final_model/"))
+    
+end_time = datetime.now()
+f = open(PATH.joinpath("training_duration.txt"), "w+")
+f.write(f"Time taken in training:{end_time - start_time}")
+f.close
+envs.close()

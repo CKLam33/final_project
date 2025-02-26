73ffa897b4a71acb8116833345b2ef6f7c0a5926
Lam Chi Kin <gary199233@hotmail.com>
2025-02-26 20:53:55 +0800

    Change Max ep. steps, LSTM hidden size, pop. size....

--- env_setup.py
+++ env_setup.py
@@ -4,20 +4,21 @@ import os
 GBA_ROM = "./GBA_ROM/Rockman Zero 3 (Japan).gba"
 GBA_SAV = "./GBA_ROM/Rockman Zero 3 (Japan).sav"
 RENDER_MODE = "rgb_array"
-FRAMESKIP = 3
-MAX_STEPS = 60 * 60 * 60 // (FRAMESKIP + 1) # 3 hours of frames / (frameskip + 1 action)
+FRAMESKIP = 0
+MAX_STEPS = 60 * 60 * 60 # 1 hours of frames
 SILENCE = True
+FRAMESTACK = 4
 RESIZE = True
 GRAYSCALE = True
 
 # NN setup
-HIDDEN_SIZE = 128
-NUM_LAYERS = 2
+HIDDEN_SIZE = 256
+NUM_LAYERS = 1
 
 # ES setup
 NUM_ACTORS = 25
-POPSIZE = 50
-GENERATIONS = 1000
+POPSIZE = NUM_ACTORS * 2
+GENERATIONS = 2500
 
 # PPO setup
 TOTAL_TIMESTEPS = MAX_STEPS * GENERATIONS

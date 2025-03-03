b505961eb91c9aae637c764e1b93f31ffff54794
Lam Chi Kin <gary199233@hotmail.com>
2025-03-03 16:06:58 +0800

    Change parameters
    Changed frameskip, framestack, popsize and gens

--- env_setup.py
+++ env_setup.py
@@ -1,13 +1,15 @@
 import os
 
+os.environ["WANDB_MODE"] = "offline"
+
 # Env setup
 GBA_ROM = "./GBA_ROM/Rockman Zero 3 (Japan).gba"
 GBA_SAV = "./GBA_ROM/Rockman Zero 3 (Japan).sav"
 RENDER_MODE = "rgb_array"
-FRAMESKIP = 0
-MAX_STEPS = 60 * 60 * 60 # 1 hours of frames
+FRAMESKIP = 1
+MAX_TIME = 60 * 60 * 60 # 1 hours of frames
 SILENCE = True
-FRAMESTACK = 4
+FRAMESTACK = 3
 RESIZE = True
 GRAYSCALE = True
 
@@ -16,10 +18,12 @@ HIDDEN_SIZE = 256
 NUM_LAYERS = 1
 
 # ES setup
-NUM_ACTORS = 25
-POPSIZE = NUM_ACTORS * 2
-GENERATIONS = 2500
+NUM_ACTORS = os.cpu_count()
+POPSIZE = NUM_ACTORS * 4
+GENERATIONS = 750
 
 # PPO setup
-TOTAL_TIMESTEPS = MAX_STEPS * GENERATIONS
+TOTAL_TIMESTEPS = (MAX_TIME // (1 + FRAMESKIP)) * POPSIZE
+N_STEPS = (MAX_TIME // (1 + FRAMESKIP)) // GENERATIONS
+BATCH_SIZE = N_STEPS
 

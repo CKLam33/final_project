e54c0a7b040b41fea3ec467c154c2fe2bd735b52
Lam Chi Kin <gary199233@hotmail.com>
2025-03-07 22:20:53 +0800

    Remove use of resolution resize and grayscale...
    Remove use of resolution resize and grayscale. Change Params for PPO

--- env_setup.py
+++ env_setup.py
@@ -5,25 +5,25 @@ os.environ["WANDB_MODE"] = "offline"
 # Env setup
 GBA_ROM = "./GBA_ROM/Rockman Zero 3 (Japan).gba"
 GBA_SAV = "./GBA_ROM/Rockman Zero 3 (Japan).sav"
-RENDER_MODE = "rgb_array"
-FRAMESKIP = 1
 MAX_TIME = 60 * 60 * 60 # 1 hours of frames
 SILENCE = True
-FRAMESTACK = 3
-RESIZE = True
-GRAYSCALE = True
+FRAMESTACK = 5
+OBS_NORM = True
+INCLUDE_LIVES = False
 
-# NN setup
+# LSTM setup
 HIDDEN_SIZE = 256
 NUM_LAYERS = 1
 
-# ES setup
-NUM_ACTORS = os.cpu_count()
-POPSIZE = NUM_ACTORS * 4
-GENERATIONS = 750
+# PGPE setup
+NUM_ACTORS = POPSIZE = os.cpu_count()
+GENERATIONS = 500
+RAD_INIT = 5
+CENTER_LR = 0.05
+STD_LR = 0.1
 
 # PPO setup
-TOTAL_TIMESTEPS = (MAX_TIME // (1 + FRAMESKIP)) * POPSIZE
-N_STEPS = (MAX_TIME // (1 + FRAMESKIP)) // GENERATIONS
-BATCH_SIZE = N_STEPS
+TOTAL_TIMESTEPS = MAX_TIME * POPSIZE
+N_STEPS = MAX_TIME // GENERATIONS
+BATCH_SIZE = 512
 
